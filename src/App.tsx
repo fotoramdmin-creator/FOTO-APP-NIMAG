0d33f22338ed40936eae390783efa250194abd01
ANTONIO RAMIREZ <fotoramdmin@gmail.com>
2026-05-08 16:21:26 +0000

    ARREGLO PARA COMMITA

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ function App() {
 
   useEffect(() => {
     // 1. Verificar si hay una sesión activa al abrir la app
-    supabase.auth.getSession().then(({ data }) => {
-      const session = data.session;
+    supabase.auth.getSession().then((res: any) => {
+      const session = res.data.session;
       setSession(session);
       setLoading(false);
     });
